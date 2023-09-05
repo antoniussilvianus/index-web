05e1c4dfff6bad26be03a9d40c5a9c9d473d6652
Antonius Silvianus <antoniussilvianus@outlook.com>
2023-09-05 18:05:20 +0700

    reformatting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import styled from "styled-components";
 
 const PizzaFrame = styled.div`
@@ -54,7 +55,6 @@ const Pizza = ({ pizza }) => {
 
   function onSave() {
     setDirty(false);
-    // make REST call
   }
 
   return (
